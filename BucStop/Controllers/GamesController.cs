d1066c9e2619f5f358de7bbf6578ea9ca7fa9725
Jacob Klucher <klucher@etsu.edu>
2023-09-28 11:44:17 -0400

    Updated game page template

--- BucStop/Controllers/GamesController.cs
+++ BucStop/Controllers/GamesController.cs
@@ -7,9 +7,19 @@ namespace BucStop.Controllers
     {
         private static List<Game> games = new List<Game>
         {
-            // Sample data - replace with database or other data source
-            new Game { Id = 1, Title = "Sample Game 1", Content = "Embed code or URL for Game 1", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-            new Game { Id = 2, Title = "Sample Game 2", Content = "Embed code or URL for Game 2", Description = "Game 2 description." }
+            // Game data
+            new Game { 
+                Id = 1, 
+                Title = "Snake", 
+                Content = "~/js/snake.js",
+                Description = "Snake - add author, description, how to play, etc..." 
+            },
+            new Game { 
+                Id = 2, 
+                Title = "Tetris", 
+                Content = "~/js/tetris.js",
+                Description = "Tetris description." 
+            }
         };
 
         // GET: Game
